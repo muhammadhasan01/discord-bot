5dde70a15bff1371528c69805cb5a61217ea3953
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-06 19:55:39 +0700

    refactor: use `msg` instead of `res`

--- tests/test_todo_handler.py
+++ tests/test_todo_handler.py
@@ -5,8 +5,8 @@ from src.todo_handler import todo_handler
 
 class TestTodoHandler(unittest.TestCase):
     def test_when_query_invalid(self):
-        res = todo_handler("$todo")
-        self.assertEqual("invalid query, $todo must have at least two argument", res)
+        msg = todo_handler("$todo")
+        self.assertEqual("invalid query, $todo must have at least two argument", msg)
 
 
 if __name__ == '__main__':
