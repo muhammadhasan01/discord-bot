74217bb6a2d845144badfb4ffd87e80b310cc713
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-06 13:48:44 +0700

    refactor: use a class of unittest instead

--- tests/test_quote.py
+++ tests/test_quote.py
@@ -5,21 +5,20 @@ from src.quote import get_quote
 SEPARATOR = '-'
 
 
-def test_get_quote_have_separator():
-    quote = get_quote()
-    assert SEPARATOR in quote
+class TestQuote(unittest.TestCase):
+    def test_get_quote_have_separator(self):
+        quote = get_quote()
+        self.assertIn(SEPARATOR, quote)
 
+    def test_get_quote_have_content(self):
+        data = get_quote().split(f' {SEPARATOR} ')
+        self.assertGreaterEqual(len(data), 1)
+        self.assertIsInstance(data[0], str)
 
-def test_get_quote_have_content():
-    data = get_quote().split(f' {SEPARATOR} ')
-    assert len(data) >= 1
-    assert isinstance(data[0], str)
-
-
-def test_get_quote_have_author():
-    data = get_quote().split(f' {SEPARATOR} ')
-    assert len(data) >= 2
-    assert isinstance(data[1], str)
+    def test_get_quote_have_author(self):
+        data = get_quote().split(f' {SEPARATOR} ')
+        self.assertGreaterEqual(len(data), 2)
+        self.assertIsInstance(data[1], str)
 
 
 if __name__ == '__main__':
