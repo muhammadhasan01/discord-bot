e9ca536436ae1af07eb15de554e3f0976581da37
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-06 20:12:11 +0700

    feat: add add task

--- src/todo_handler.py
+++ src/todo_handler.py
@@ -1,7 +1,17 @@
+from mysql.connector import MySQLConnection
+
+from src.db import insert_todo_task
+
 INVALID_QUERY_ARGUMENT = "invalid query, $todo must have at least two argument"
 
 
-def todo_handler(content: str):
+def todo_handler(db: MySQLConnection, content: str):
     data = content.strip().split()
     if len(data) < 2:
         return INVALID_QUERY_ARGUMENT
+
+    if data[1] == "add":
+        task = " ".join(data[2:])[1:-1]
+        row_id = insert_todo_task(db, task)
+        return f'Task of "{task}" was successfully added with an id={row_id}'
+
--- tests/test_todo_handler.py
+++ tests/test_todo_handler.py
@@ -1,12 +1,41 @@
+import os
 import unittest
 
+from dotenv import load_dotenv
+
+from src.db import connect_db, create_table_todos
 from src.todo_handler import todo_handler
 
 
 class TestTodoHandler(unittest.TestCase):
+    db = None
+
+    @classmethod
+    def setUpClass(cls):
+        load_dotenv()
+        cls.db = connect_db(
+            host=os.getenv("TEST_HOST"),
+            user=os.getenv("TEST_USER"),
+            password=os.getenv("TEST_PASSWORD"),
+            port=os.getenv("TEST_PORT"),
+            database=os.getenv("TEST_DATABASE")
+        )
+        create_table_todos(cls.db)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.db.close()
+
     def test_when_query_invalid(self):
-        msg = todo_handler("$todo")
+        msg = todo_handler(self.db, "$todo")
         self.assertEqual("invalid query, $todo must have at least two argument", msg)
+
+    def test_add_task_success(self):
+        msg = todo_handler(self.db, '$todo add "task 1"')
+        cursor = self.db.cursor()
+        cursor.execute('SELECT id FROM todos ORDER BY id DESC LIMIT 1')
+        res = cursor.fetchone()
+        self.assertEqual(f'Task of "task 1" was successfully added with an id={res[0]}', msg)
 
 
 if __name__ == '__main__':
