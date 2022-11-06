9599c2a19b1101de9cb1d5b5e02165d6b3601c61
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-06 14:38:38 +0700

    refactor: use set up and tear down class

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,31 +4,33 @@ from src.db import connect_db, create_table_todos, insert_todo_task, show_todo_t
 
 
 class MyTestCase(unittest.TestCase):
+    db = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db = connect_db()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.db.close()
+
     def test_connect_db(self):
-        mydb = connect_db()
-        self.assertTrue(mydb.is_connected())
-        mydb.close()
+        self.assertTrue(self.db.is_connected())
 
     def test_create_table_todos(self):
-        mydb = connect_db()
-        create_table_todos(mydb)
-        cursor = mydb.cursor()
+        create_table_todos(self.db)
+        cursor = self.db.cursor()
         cursor.execute("SHOW TABLES LIKE 'todos'")
         self.assertGreaterEqual(len(cursor.fetchall()), 1)
-        mydb.close()
 
     def test_insert_todo_task(self):
-        mydb = connect_db()
-        row_id = insert_todo_task(mydb, "task")
+        row_id = insert_todo_task(self.db, "task")
         self.assertIsInstance(row_id, int)
         self.assertGreaterEqual(row_id, 1)
-        mydb.close()
 
     def test_show_todo_task(self):
-        mydb = connect_db()
-        result = show_todo_task(mydb)
+        result = show_todo_task(self.db)
         self.assertIsInstance(result, list)
-        mydb.close()
 
 
 if __name__ == '__main__':
