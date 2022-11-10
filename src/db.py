c6dd25ee1a5ea2b37f06082a1e22f6f2c6e27e68
Muhammad Hasan <muhammadhasan50@gmail.com>
2022-11-11 06:25:11 +0700

    feat: add logging on `db.py`

--- src/db.py
+++ src/db.py
@@ -1,8 +1,13 @@
 import mysql.connector
 from mysql.connector import MySQLConnection
 
+from src.utils.logger import create_logger
+
+logger = create_logger()
+
 
 def connect_db(host: str, user: str, password: str, port: str, database: str):
+    logger.info(f'connecting to database {database} on {host}:{port} as user {user}')
     return mysql.connector.connect(
         host=host,
         user=user,
@@ -13,6 +18,7 @@ def connect_db(host: str, user: str, password: str, port: str, database: str):
 
 
 def create_table_todos(mydb: MySQLConnection):
+    logger.info("creating table todo")
     cursor = mydb.cursor()
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS todos (
@@ -25,6 +31,7 @@ def create_table_todos(mydb: MySQLConnection):
 
 
 def insert_todo_task(mydb: MySQLConnection, task: str):
+    logger.info(f'inserting task={task}')
     cursor = mydb.cursor()
 
     sql = "INSERT INTO todos (content, is_done) VALUES (%s, %s)"
@@ -32,16 +39,20 @@ def insert_todo_task(mydb: MySQLConnection, task: str):
 
     cursor.execute(sql, val)
     mydb.commit()
-    return cursor.lastrowid
+    res = cursor.lastrowid
+    logger.info(f'task of "{task}" with id={res} was created')
+    return res
 
 
 def show_todo_tasks(mydb: MySQLConnection, limit: int = 5):
+    logger.info(f'fetching to view todo with limit={limit}')
     cursor = mydb.cursor()
     cursor.execute(f"SELECT * FROM todos LIMIT {limit}")
     return cursor.fetchall()
 
 
 def update_task_status(mydb: MySQLConnection, row_id: int, status: int):
+    logger.info(f'updating task with id={row_id} to status={status}')
     cursor = mydb.cursor()
 
     sql = "UPDATE todos SET is_done = %s WHERE id = %s"
@@ -50,21 +61,32 @@ def update_task_status(mydb: MySQLConnection, row_id: int, status: int):
     cursor.execute(sql, val)
     mydb.commit()
 
-    return cursor.rowcount
+    res = cursor.rowcount
+    if res == 0:
+        logger.warn(f'updating task failed for id={row_id} and status={status}')
+
+    return res
 
 
 def select_task(mydb: MySQLConnection, row_id: int):
+    logger.info(f'selecting task with id={row_id}')
     cursor = mydb.cursor()
     cursor.execute(f"SELECT * FROM todos WHERE id={row_id}")
     return cursor.fetchone()
 
 
 def delete_task(mydb: MySQLConnection, row_id: int):
+    logger.info(f'deleting task with id={row_id}')
     cursor = mydb.cursor()
     cursor.execute(f"DELETE FROM todos WHERE id={row_id}")
+    res = cursor.rowcount
+    if res == 0:
+        logger.warn(f'failed on deleting task with id={row_id}')
+
     return cursor.rowcount
 
 
 def clear_todo_tasks(mydb: MySQLConnection):
+    logger.info('clearing todo tasks')
     cursor = mydb.cursor()
     cursor.execute("TRUNCATE TABLE todos")
